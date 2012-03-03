fb28b9e621480fb976ad2990d40d0f71855c77ea
Julio Cesar de Souza <julio@julioc.me>
2012-03-03 02:51:08 -0300

    Implemented Particle class

--- particle.cpp
+++ particle.cpp
@@ -1,5 +1,7 @@
 #include "particle.h"
 
-Particle::Particle()
-{
+Particle::Particle(int l, const Vector4D &p, const Vector4D &v):
+    lifetime(l),
+    position(p),
+    velocity(v) {
 }
--- particle.h
+++ particle.h
@@ -1,10 +1,23 @@
 #ifndef PARTICLE_H
 #define PARTICLE_H
 
+#include "vector4d.h"
+
 class Particle
 {
 public:
-    Particle();
+    // Exposed properties for ease of access
+    int lifetime;
+    Vector4D position;
+    Vector4D velocity;
+    //@TODO: Add color
+
+    Particle(int l, const Vector4D &p, const Vector4D &v);
+
+private:
+    // Do not copy me!
+    Particle(const Particle &);
+    Particle& operator=(const Particle &);
 };
 
 #endif // PARTICLE_H

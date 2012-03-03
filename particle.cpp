725d7b03b61e1ab24967330164e38c04b476f683
Julio Cesar de Souza <julio@julioc.me>
2012-03-03 03:17:29 -0300

    Fixed whitespace

--- particle.cpp
+++ particle.cpp
@@ -1,6 +1,6 @@
 #include "particle.h"
 
-Particle::Particle(int l, const Vector4D &p, const Vector4D &v):
+Particle::Particle(int l, const Vector4D &p, const Vector4D &v) :
     lifetime(l),
     position(p),
     velocity(v) {

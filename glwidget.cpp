a9b4fe7dc1e9d45d65f80a30b77c37d653426f11
Julio Cesar de Souza <julio@julioc.me>
2012-03-03 04:51:04 -0300

    Added particle emitting

--- glwidget.cpp
+++ glwidget.cpp
@@ -1,10 +1,22 @@
 #include "glwidget.h"
 
+#include <QTimer>
+
+#include "emitter.h"
+#include "dummyrenderer.h"
+
 GLWidget::GLWidget(QWidget *parent) :
-    QGLWidget(parent) {
+    QGLWidget(parent),
+    _timer(NULL),
+    _elapsedTimer(NULL),
+    _lastFrameTime(0),
+    _emitter(NULL) {
 }
 
 GLWidget::~GLWidget() {
+    if(_timer) delete _timer;
+    if(_elapsedTimer) delete _elapsedTimer;
+    if(_emitter) delete _emitter;
 }
 
 QSize GLWidget::minimumSizeHint() const {
@@ -15,15 +27,43 @@ QSize GLWidget::sizeHint() const {
     return QSize(800, 600);
 }
 
+void GLWidget::animate()
+{
+    int current = _elapsedTimer->elapsed();
+    float elapsed = (float)(current - _lastFrameTime) / 1000.0f;
+    _lastFrameTime = current;
+
+    _emitter->update(elapsed);
+
+    updateGL();
+}
+
 void GLWidget::initializeGL() {
     glClearColor(.0, .0, .0, 1.0);
     glShadeModel(GL_FLAT);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
+
+    // Create the elapsed timer
+    _elapsedTimer = new QElapsedTimer();
+    _elapsedTimer->start();
+
+    // Fire the animation timer
+    if(_timer != NULL) delete _timer;
+    _timer = new QTimer(this);
+    connect(_timer, SIGNAL(timeout()), this, SLOT(animate()));
+    _timer->start(0);
+
+    //@TODO: do time management in a better way
+
+    _emitter = new Emitter(Vector4D(), Vector4D(1, 1, 1, 1));
+    _emitter->renderer(new DummyRenderer());
 }
 
 void GLWidget::paintGL() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    _emitter->draw();
 }
 
 void GLWidget::resizeGL(int w, int h) {
--- glwidget.h
+++ glwidget.h
@@ -2,6 +2,9 @@
 #define GLWIDGET_H
 
 #include <QGLWidget>
+#include <QElapsedTimer>
+
+class Emitter;
 
 class GLWidget : public QGLWidget {
     Q_OBJECT
@@ -13,11 +16,21 @@ public:
     QSize minimumSizeHint() const;
     QSize sizeHint() const;
 
+public slots:
+    void animate();
+
 protected:
     void initializeGL();
     void paintGL();
     void resizeGL(int width, int height);
     
+private:
+    QTimer* _timer;
+    QElapsedTimer* _elapsedTimer;
+
+    int _lastFrameTime;
+
+    Emitter* _emitter;
 };
 
 #endif // GLWIDGET_H

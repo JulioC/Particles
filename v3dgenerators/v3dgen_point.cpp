fc0ef3437d437c499d62c62ec943dddf7060722f
Julio Cesar de Souza <julio@julioc.me>
2012-03-06 20:31:15 -0300

    Fixed bad renaming on  V3DGen_Point (oops)

--- v3dgenerators/v3dgen_point.cpp
+++ v3dgenerators/v3dgen_point.cpp
@@ -1,13 +1,13 @@
 #include "v3dgen_point.h"
 
-V3DGen_generate::V3DGen_generate(const Vector3D &position) :
+V3DGen_Point::V3DGen_Point(const Vector3D &position) :
   V3DGenerator(),
   _position(position) {
 }
 
-V3DGen_generate::~V3DGen_generate() {
+V3DGen_Point::~V3DGen_Point() {
 }
 
-Vector3D V3DGen_generate::generate() {
+Vector3D V3DGen_Point::generate() {
   return _position;
 }
--- v3dgenerators/v3dgen_point.h
+++ v3dgenerators/v3dgen_point.h
@@ -1,12 +1,12 @@
-#ifndef V3DGEN_generate_H
-#define V3DGEN_generate_H
+#ifndef V3DGEN_POINT_H
+#define V3DGEN_POINT_H
 
 #include "v3dgenerator.h"
 
-class V3DGen_generate : public V3DGenerator {
+class V3DGen_Point : public V3DGenerator {
 public:
-  V3DGen_generate(const Vector3D &position);
-  virtual ~V3DGen_generate();
+  V3DGen_Point(const Vector3D &position);
+  virtual ~V3DGen_Point();
 
   virtual Vector3D generate();
 
@@ -14,4 +14,4 @@ protected:
   Vector3D _position;
 };
 
-#endif // V3DGEN_generate_H
+#endif // V3DGEN_POINT_H
